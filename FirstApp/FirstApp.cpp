c926c06ba2fa6a7de80da8312ec01dcdeb3b6a73
Boris_ty06 <Boris_ty06@21-7>
2024-11-06 18:50:26 +0200

    Add project files.

---
+++ FirstApp/FirstApp.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //1
+    cout << "Borisenko\n";
+    cout << "Yaroslav\n";
+    cout << "Evgenievich\n";
+
+    //2
+    cout << "To be\n";
+    cout << "\tor not\n";
+    cout << "\t\tto be?\n";
+
+    cout << "\t\t\tWilliam Shakespeare";
+}
+
